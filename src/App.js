7a6b47132bc4b90bef4278a2330f54e8a0171191
Kumar <santraj.kumar@accenture.com>
2019-12-12 17:04:23 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, useState } from 'react';
 import List from './List';
 import List2 from './List2';
+import InfiniteUsers from './InfiniteUsers';
 
 class App extends Component {
   render() {
@@ -25,11 +26,11 @@ class App extends Component {
         </div>
         <div className="row">
           <div className="col-6 justify-content-center my-5">
-            <List />
+            <InfiniteUsers />
           </div>
-          <div className="col-6 justify-content-center my-5">
+          {/* <div className="col-6 justify-content-center my-5">
             <List2 />
-          </div>
+          </div> */}
         </div>
       </div>
     );
@@ -37,3 +38,67 @@ class App extends Component {
 }
 
 export default App;
+
+// import React, { Component } from "react";
+// // import logo from "./logo.svg";
+// // import "./App.css";
+    
+//     class App extends Component {
+//       constructor(props) {
+//         super(props);
+//         this.state = {
+//           items: 20,
+//           loading: false
+//         };
+//       }
+//       componentDidMount() {
+//         // Detect when scrolled to bottom.
+//         this.refs.myscroll.addEventListener("scroll", () => {
+//           if (
+//             this.refs.myscroll.scrollTop + this.refs.myscroll.clientHeight >=
+//             this.refs.myscroll.scrollHeight
+//           ) {
+//             this.loadMore();
+//           }
+//         });
+//       }
+    
+//       showItems() {
+//         var items = [];
+//         for (var i = 0; i < this.state.items; i++) {
+//           items.push(<li key={i}>Item {i}</li>);
+//         }
+//         return items;
+//       }
+    
+//       loadMore() {
+//         this.setState({ loading: true });
+//         setTimeout(() => {
+//           this.setState({ items: this.state.items + 20, loading: false });
+//         }, 2000);
+//       }
+    
+//       render() {
+//         return (
+//           <div
+//             ref="myscroll"
+//             style={{ height: "420px", overflow: "auto" }}
+//           >
+//             <header className="App-header">
+//               <h1 className="App-title">Welcome to React</h1>
+//             </header>
+//             <ul>
+//               {this.showItems()}
+//             </ul>
+//             {this.state.loading
+//               ? <p className="App-intro">
+//                   loading ...
+//                 </p>
+//               : ""}
+    
+//           </div>
+//         );
+//       }
+//     }
+    
+//     export default App;
